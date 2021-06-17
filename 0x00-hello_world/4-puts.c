9eb933ae911f633b8111e24c747678b99dfa654f
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-17 11:32:45 -0700

    S

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-int puts("Programming is like building a multilingual puzzle");
+#include<studio.h>
+int main(){
+	char MyStr="Programming is like building a multilingual puzzle";
+	puts(MyStr);
+	return 0;
+}
