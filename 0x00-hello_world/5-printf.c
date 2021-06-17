3d9e2c272a75e4338313ff4ac1ed316d94951521
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-17 12:32:25 -0700

    R

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(void){
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * main - printf and return zero
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
