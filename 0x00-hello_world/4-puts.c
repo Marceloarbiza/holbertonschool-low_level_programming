2f35d1cb34facf1f540b2ade7b6e860068815d03
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-17 12:01:59 -0700

    E

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/**
+/*
  * Use the function puts
  * You are not allowed to use printf
  * Your program should end with the value 0
- **/
-int main(void)
+*/
+int main(void) /*int main */
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
