4df1781d0910acb0ce3c7e039888a65d63086416
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-17 11:53:47 -0700

    W

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include<studio.h>
-int main(void)
-{
-	char MyStr[] = "Programming is like building a multilingual puzzle";
-
-	puts(MyStr);
-	
-	return (0);
+int main(void){
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
 
