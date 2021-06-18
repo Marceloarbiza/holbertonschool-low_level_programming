d12f1c2269a1c7dbe848246e2552c222ad06ae7f
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 09:48:12 -0700

    R

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+        char c, line;
+        line = '\n';
+        for (c = 'a' ; c <= 'z' ; c++){	
+		if(c == 'e'){
+			continue;
+		}else if(c == 'q'){
+			continue;
+		}else{
+			putchar (c);
+		}
+        }
+        putchar (line);
+        return (0);
+}
