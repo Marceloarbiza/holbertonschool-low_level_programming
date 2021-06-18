81821ca045bf77a4d6b701711b14ccceab99b330
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 10:14:57 -0700

    T

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+        int i = 0;
+        for (i = 0; i <= 9; i++){
+                putchar(i + '0');
+        }
+        putchar('\n');
+        return(0);
+}
