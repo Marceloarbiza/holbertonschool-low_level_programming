24b6d5c87cdf8a9146cfa11b207cb442c81d1323
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 11:34:19 -0700

    T

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - print alpha
+ *
+ * Return: zero
+ */
+int main(void)
+{
 	char c;
-	for(c = 'z'; c >= 'a'; c--){
-		putchar(c);
-	}
-	putchar('\n');
-	return(0);
+		for (c = 'z'; c >= 'a'; c--)
+		{
+			putchar(c);
+		}
+	putchar ('\n');
+	return (0);
 }
