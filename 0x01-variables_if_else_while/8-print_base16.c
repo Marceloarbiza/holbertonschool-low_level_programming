fd25f14b033a0e5b2e9364223f4549f3a34d8332
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 11:29:06 -0700

    R

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-int main(void){
-        int c; 
+/**
+ * main - print base
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int c;
 	char a, line;
-        line = '\n';
-        for (c = 0 ; c <= 9 ; c++)
-        {
-                putchar(c + '0');
-        }
-        for (a = 'a'; a <= 'f'; a++)
-        {
-                putchar(a);
-        }
-        putchar(line);
-        return(0);
+
+	line = '\n';
+		for (c = 0 ; c <= 9 ; c++)
+		{
+			putchar(c + '0');
+		}
+		for (a = 'a'; a <= 'f' ; a++)
+		{
+			putchar(a);
+		}
+		putchar(line);
+		return (0);
 }
