1473fb1fa1af1a1d1ccf4d541f3abe300196d361
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 11:40:39 -0700

    R

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-int main(void){
-       	int i = 0;
-	for (i = 0; i <= 9; i++){
-		putchar(i + '0');	
+/**
+ * main - print num
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
