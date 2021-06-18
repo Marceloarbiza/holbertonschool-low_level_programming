089bc1f68ace33f47a39c1ea0d005e2c1f794861
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-18 11:36:48 -0700

    R

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main(void){
-        int i = 0;
-        for (i = 0; i <= 9; i++){
-                putchar(i + '0');
-        }
-        putchar('\n');
-        return(0);
+/**
+ * main - print number
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 0;
+		for (i = 0; i <= 9; i++)
+		{
+			putchar(i + '0');
+		}
+	putchar ('\n');
+	return (0);
 }
