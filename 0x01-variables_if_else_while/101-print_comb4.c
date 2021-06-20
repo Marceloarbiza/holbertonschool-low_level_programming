1928700aa506393bcf919cc2410488815244a5c1
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-20 09:56:51 -0700

    R

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,33 +6,33 @@
  */
 int main(void)
 {
-        int i;
-        int z;
+	int i;
+	int z;
 	int c;
 
-        for (i = 0 ; i <= 9 ; i++)
-        {
-                for (z = 0 ; z <= 9 ; z++)
-                {
-                        if (i != z &&  z > i)
-                        {
-				for(c = 0 ; c <= 9 ; c++)
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (z = 0 ; z <= 9 ; z++)
+		{
+			if (i != z &&  z > i)
+			{
+				for (c = 0 ; c <= 9 ; c++)
 				{
 					if (c != z && c != i && c > z)
 					{
-                                		putchar(i + '0');
-                                		putchar(z + '0');
+						putchar(i + '0');
+						putchar(z + '0');
 						putchar(c + '0');
-                                	if (i + z + c < 24)
-                                	{
-                                        	putchar(',');
-                                        	putchar(' ');
+					if (i + z + c < 24)
+					{
+						putchar(',');
+						putchar(' ');
 					}
-                                	}
+					}
 				}
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
