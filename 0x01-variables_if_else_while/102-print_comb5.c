95c0ecdce2092893c4378163b27a73b1899a734d
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-20 12:01:25 -0700

    E

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,40 +6,36 @@
  */
 int main(void)
 {
-        int i;
-        int z;
+	int i;
+	int z;
 	int c;
 	int d;
 
-        for (i = 0 ; i <= 9 ; i++)
-        {
-                for (z = 0 ; z <= 9 ; z++)
-                {
-              		for (c = 0 ; c <= 9 ; c++)
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (z = 0 ; z <= 9 ; z++)
+		{
+			for (c = 0 ; c <= 9 ; c++)
 			{
 				for (d = 0 ; d <= 9 ; d++)
 				{
-						if (c * 10 + d > i * 10 + z)
-						{	 
-                                                	putchar(i + '0');
-                                                	putchar(z + '0');
-							putchar(' ');
-                                               		putchar(c + '0');
-							putchar(d + '0');
-						
-                                        	if (i != 9 || z != 8 || c != 9 || d != 9)
-                                        	{
-                                                	putchar(',');
-                                                	putchar(' ');
-                                        	}
-						}
-				
-						
-                                        	
+					if (c * 10 + d > i * 10 + z)
+					{
+						putchar(i + '0');
+						putchar(z + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+					if (i != 9 || z != 8 || c != 9 || d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
 				}
-			}	
-                }
-        }
-        putchar('\n');
-        return (0);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
