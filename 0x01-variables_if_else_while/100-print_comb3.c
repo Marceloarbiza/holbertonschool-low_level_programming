93f5cf26a8222da2157629b287e5dc3d82275c2e
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-20 09:31:43 -0700

    W

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - iqual
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	int z;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (z = 0; z <= 9 ; z++){
+			if (i != z && z > i){
+			putchar(i + '0');
+			putchar(z + '0');
+			if(i+z < 17){
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
