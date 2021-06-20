0d01e0e09682c7bb5e7ce6abcb57e9288386aafd
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-20 11:01:54 -0700

    F

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,35 +8,35 @@ int main(void)
 {
         int i;
         int z;
-        int c;
+	int c;
 	int d;
 
         for (i = 0 ; i <= 9 ; i++)
         {
                 for (z = 0 ; z <= 9 ; z++)
                 {
-                        
-                        
-                                for (c = 0 ; c <= 9 ; c++)
-                                {
-					for (d = 0 ; d <= 9 ;  d++)
-					{
-                                        	if (c + d > i + z)
-                                        	{
+              		for (c = 0 ; c <= 9 ; c++)
+			{
+				for (d = 0 ; d <= 9 ; d++)
+				{		if (c + d > i + z)
+						{	
                                                 	putchar(i + '0');
                                                 	putchar(z + '0');
 							putchar(' ');
                                                		putchar(c + '0');
 							putchar(d + '0');
-                                        	if (i + z + c + d < 35)
+						
+                                        	if (i != 9 || z != 8 || c != 9 || d != 9)
                                         	{
                                                 	putchar(',');
                                                 	putchar(' ');
                                         	}
-                                        	}
-					}
-                                }
-
+						}
+				
+						
+                                        	
+				}
+			}	
                 }
         }
         putchar('\n');
