ed92c586636afd8ef6ea30b97279451de357013a
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-20 09:36:27 -0700

    R

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,20 @@ int main(void)
 {
 	int i;
 	int z;
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (z = 0; z <= 9 ; z++){
-			if (i != z && z > i){
-			putchar(i + '0');
-			putchar(z + '0');
-			if(i+z < 17){
-				putchar(',');
-				putchar(' ');
-			}
+		for (z = 0; z <= 9 ; z++)
+		{
+			if (i != z && z > i)
+			{
+				putchar(i + '0');
+				putchar(z + '0');
+				if (i + z < 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
