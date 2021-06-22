9340026c2dd312fc658b6bd8f59e58f04cd35c5a
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-22 09:47:19 -0700

    R

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - alphabet x ten
+ *
+ * Return: alphabet x ten
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	while (i <= 10)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
