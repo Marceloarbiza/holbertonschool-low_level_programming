ba521722de19ca5b1a9d2b4721f6de2793e4ae12
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-22 14:31:04 -0700

    F

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * times_table
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int f;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (f = 0 ; f < 10 ; f++)
+		{
+			int g = i * f;
+			if (g >= 10)
+			{
+				_putchar((g / 10) + '0');
+			}
+			else if (g < 10 && f > 0)
+			{
+				_putchar(' ');
+			}
+			_putchar((g % 10) + '0');
+			if (f < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}	
+}
