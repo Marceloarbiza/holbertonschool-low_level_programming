a511fc430578064d3d17ba0514dd17f97d924d0d
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-22 15:30:57 -0700

    R

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,83 @@
+#include "holberton.h"
+/**
+ * print_to_98 - print
+ * @n: character
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+	int g;
+	
+	if (n <= 98){
+		for (i = n ; i <= 98 ; i++)
+		{
+			if (i < 10)
+			{
+				if(i >= 0)
+				{
+					_putchar((i % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else if (i < 0) 
+				{
+					g = i * -1;
+					if (g < 10)
+					{
+						_putchar('-');
+						_putchar((g % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar('-');
+						_putchar((g / 10) + '0');
+						_putchar((g % 10) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}				
+				}
+			}
+			else if (i == 98)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+        if (n > 98){
+                for (i = n ; i >= 98 ; i--)
+                {
+			if (i >= 100)
+			{
+                        	_putchar((i / 100) + '0');
+				_putchar((i / 10) % 10 + '0');
+                        	_putchar((i % 10) + '0');
+                        	_putchar(',');
+                        	_putchar(' ');
+			}
+			else if (i == 98)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+                }
+        }
+	_putchar('\n');
+}
