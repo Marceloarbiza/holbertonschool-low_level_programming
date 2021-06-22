00e337d7d19a670fcb1f4d23f5b51bc079a81e59
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-22 09:20:43 -0700

    E

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - alphabet
+ *
+ * Return: zero
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
