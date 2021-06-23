ce80b68b5b7ec6b83a8d4046af38c68d8649386a
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-22 17:09:57 -0700

    F

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,90 +3,36 @@
  * print_to_98 - print
  * @n: character
  *
- */
+ */	
 void print_to_98(int n)
 {
-	int i;
-	int g;
-	
-	if (n <= 98){
-		for (i = n ; i <= 98 ; i++)
+	if (n < 98)
+	{
+		while (n <= 98)
 		{
-			if (i < 10)
+			printf("%d", n);
+			if (n != 98)
 			{
-				if(i >= 0)
-				{
-					_putchar((i % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if (i < 0) 
-				{
-					g = i * -1;
-					if (g >= 100)
-					{
-						_putchar('-');
-						_putchar((g / 100)+ '0');
-						_putchar((g / 10) % 10 + '0');
-						_putchar((g % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-					if (g < 10)
-					{
-						_putchar('-');
-						_putchar((g % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (g >= 10 && g < 100)
-					{
-						_putchar('-');
-						_putchar((g / 10) + '0');
-						_putchar((g % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}				
-				}
+				printf(", ");
 			}
-			else if (i == 98)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			}
+			n++;
 		}
 	}
-        if (n > 98){
-                for (i = n ; i >= 98 ; i--)
-                {
-			if (i >= 100)
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
 			{
-                        	_putchar((i / 100) + '0');
-				_putchar((i / 10) % 10 + '0');
-                        	_putchar((i % 10) + '0');
-                        	_putchar(',');
-                        	_putchar(' ');
+				printf(", ");
 			}
-			else if (i == 98)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-                }
-        }
-	_putchar('\n');
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
