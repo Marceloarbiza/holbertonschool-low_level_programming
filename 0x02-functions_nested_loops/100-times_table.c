6eb8e2075d432b173c83938c231a97c075172469
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-23 12:00:29 -0700

    R

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,46 +8,42 @@ void print_times_table(int n)
 {
 	long i, f;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
-	}
-	else if (n < 15 && n > 0)
+	if (n < 15 && n > 0)
 	{
 		for (i = 0 ; i <= n ; i++)
-	{
-		for (f = 0 ; f <= n ; f++)
 		{
-			long g = i * f;
+			for (f = 0 ; f <= n ; f++)
+			{
 
-			if (g >= 10 && g < 100)
-			{
-				_putchar(' ');
-				_putchar((g / 10) + '0');
+					long g = i * f;
+
+					if (g >= 10 && g < 100)
+					{
+						_putchar(' ');
+						_putchar((g / 10) + '0');
+					}
+					else if (g < 10 && f > 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					if (g >= 100)
+					{
+						_putchar((g / 100) + '0');
+						_putchar(((g / 10) % 10) + '0');
+						_putchar((g % 10) + '0');
+					}
+					else
+					{
+						_putchar((g % 10) + '0');
+					}
+					if (f < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 			}
-			else if (g < 10 && f > 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			if (g >= 100)
-			{
-				_putchar((g / 100) + '0');
-				_putchar((g / 10) % 10 + '0');
-				_putchar((g % 10) + '0');
-			}
-			else
-			{
-				_putchar((g % 10) + '0');
-			}
-			if (f < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
