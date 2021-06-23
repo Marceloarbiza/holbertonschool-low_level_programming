157387f51d584265e21072a08c8642505b5ad9a3
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-23 12:11:17 -0700

    F

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,42 +6,32 @@
  */
 void print_times_table(int n)
 {
-	long i, f;
+	int a, b, c;
 
-	if (n < 15 && n > 0)
+	if ((n >= 0) && (n <= 15))
 	{
-		for (i = 0 ; i <= n ; i++)
+		for (a = 0; a <= n; a++)
 		{
-			for (f = 0 ; f <= n ; f++)
+			for (b = 0; b <= n; b++)
 			{
-
-					long g = i * f;
-
-					if (g >= 10 && g < 100)
-					{
-						_putchar(' ');
-						_putchar((g / 10) + '0');
-					}
-					else if (g < 10 && f > 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					if (g >= 100)
-					{
-						_putchar((g / 100) + '0');
-						_putchar(((g / 10) % 10) + '0');
-						_putchar((g % 10) + '0');
-					}
+				c = a * b;
+				if (b > 0)
+				{
+					_putchar(' ');
+					if (c > 99)
+						_putchar((c / 100) + '0');
 					else
-					{
-						_putchar((g % 10) + '0');
-					}
-					if (f < n)
-					{
-						_putchar(',');
 						_putchar(' ');
-					}
+					if (c > 9)
+						_putchar(((c / 10) % 10) + '0');
+					else
+						_putchar(' ');
+				}
+				_putchar((c % 10) + '0');
+				if (b < n)
+				{
+					_putchar(',');
+				}
 			}
 			_putchar('\n');
 		}
