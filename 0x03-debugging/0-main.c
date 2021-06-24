cb9307d997d1bd43ae455e86e35d3029c79358ab
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-24 06:08:44 -0700

    R

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main - positive negative
- *
- */
-void main(int n)
-{
-                srand(time(0));
-                n = rand() - RAND_MAX / 2;
 
-                if (n < 0)
-                {
-                        printf("%d is negative\n", n);
-                }
-                else if (n > 0)
-                {
-                        printf("%d is positive\n", n);
-                }
-                else
-                {
-                        printf("%d is zero\n", n);
-                }
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
