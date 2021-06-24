c881efe300e8125be1392caa4a8125a84802401a
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-24 10:36:07 -0700

    E

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* -_isdigit
+* @c: character
+*
+* Return: int
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
