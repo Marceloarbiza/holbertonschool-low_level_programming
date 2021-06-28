0e44f8f6934e0d0c370a6abef71811df8d2f5aaa
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-28 12:57:16 -0700

    F

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,7 +9,14 @@ void _puts(char *str)
 	int i;
 
 	for (i = 0 ; *(str + i) != '\0' ; i++)
-	{
+	{	
+		if (*(str + i) == '\0')
+		{
+			continue;
+		}
+		else
+		{
 		_putchar(*(str + i));
+		}
 	}
 }
