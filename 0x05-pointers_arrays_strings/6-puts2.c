18c7acfe1d58f96d7db2f836eb238cc60d5af926
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-28 14:42:24 -0700

    E

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - puts2
+ * @str:character
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; *(str + i) != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
+}
