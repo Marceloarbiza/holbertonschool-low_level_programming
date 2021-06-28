3f8a4600951e7b8691c8feb5e2f89adfc8fc34b3
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-28 13:41:59 -0700

    E

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * _strlen - lenght
+ * @s:char
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+        int i;
+
+        for (i = 0 ; *(s + i) != '\0' ; i++)
+        {
+                continue;
+        }
+
+        return (i);
+}
+/**
+ * print_rev - reverse
+ * @s:character
+ *
+ */
+void print_rev(char *s)
+{
+        int i;
+        int j;
+
+        j = _strlen(s);
+
+        for (i = j ; *(s + i - 1) != '\0' ; i--)
+        {
+                _putchar(*(s + i - 1));
+        }
+        _putchar('\n');
+}
+/**
+ * rev_string - rev string
+ * @s:character
+ *
+ */
+void rev_string(char *s)
+{
+	print_rev(s);
+}
