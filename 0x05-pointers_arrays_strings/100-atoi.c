f748255e1157b6d40925e2387a7a26c9fdf30738
Marcelo Arbiza <3338@holbertonschool.com>
2021-06-29 08:01:41 -0700

    F

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+ * _atoi - infinite
+ * @s:char
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i;
+	int j;
+	int k;
+	int neg;
+	char sig;
+	char tot[999];
+	char num[999];
+
+	for (i = 0 ; *(s + i) != '\0' ; i++)
+	{
+		if (*(s + i) == "-")
+		{
+			neg++;	
+		}
+	}
+	if (neg % 2 == 0)
+	{
+		sig = "+";
+	}
+	else
+	{
+		sig = "-";
+	}
+	
+	i = 0;
+	j = 1;
+
+	for (i = 0 ; *(s +´i) != '\0' ; i++)
+	{
+		if (*(s + i) > 47 && *(s + i) < 58)
+		{
+			num[j] = *(s + i);
+			j++;		
+		}
+	}
+	
+	tot[0] = sig;
+	for (k = 1 ; k <= j ; k++)
+	{
+		tot[k] = num[k - 1];
+	}
+	return (tot + '0');
+}
