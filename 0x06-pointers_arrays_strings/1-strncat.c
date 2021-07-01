5d076cc1696764ed7296144a71d56f945954fadc
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-01 11:45:12 -0700

    D

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,10 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	p = &dest;
 
 	for (i = 0 ; *(dest + i) != '\0' ; i++)
-	{
-	}
-
-	dest[i] = ' ';
+		;
 
 	for (j = 0 ; *(src + j) != '\0' ; j++)
 	{
@@ -36,5 +33,7 @@ char *_strncat(char *dest, char *src, int n)
 		*(dest + i) = *(src + j);
 		i++;
 	}
+
+	dest[i + j] = '\0';
 	return (*p);
 }
