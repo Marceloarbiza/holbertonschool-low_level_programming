e11b9f0fc8b37b3e49026b2bc8f4b0220554efd3
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-01 13:41:40 -0700

    D

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *rot13 - rot
+ * @c:char
+ *
+ * Return: char
+ */
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char s1[] = "abcdefghijklmABCDEFGHIJKLM";
+	char s2[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	for (i = 0 ; c[i] != '\0' ; i++)
+	{
+		for (j = 0 ; s1[j] != '\0' ; j++)
+		{
+			if (c[i] == s1[j])
+			{
+				c[i] = s2[j];
+			}
+
+		}
+	}
+	return (c);
+}
