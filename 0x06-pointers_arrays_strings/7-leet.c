386a9d4aa5d1747e387bd8bfb604b1def5d735e7
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-01 11:36:50 -0700

    F

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,14 +13,14 @@ char *leet(char *c)
 	char numeros[] = "4433007711";
 
 	for (i = 0 ; *(c + i) != '\0' ; i++)
-	{	
+	{
 		for (j = 0 ; letras[j] != '\0' ; j++)
 		{
 			if (*(c + i) == letras[j])
 			{
 			*(c + i) = numeros[j];
 			}
-		}	
+		}
 	}
 	return (c);
 }
