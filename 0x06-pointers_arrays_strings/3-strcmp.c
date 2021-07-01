5cb1d74f6822607fba4192f3288ee05b62c15699
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-01 11:04:51 -0700

    F

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int j;
 	int res;
-	
+
 	j = 0;
 
 	for (i = 0 ; *(s1 + i) != '\0' ; i++)
@@ -22,11 +22,11 @@ int _strcmp(char *s1, char *s2)
 			break;
 		}
 		else if (*(s1 + i) == *(s2 + j))
-	{	
+	{
 			res = 0;
 			j++;
-		}	
+		}
 	}
-	return(res);
+	return (res);
 
 }
