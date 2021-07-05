e385efaf8a806fb45fb1cfc40c3ebdd4725291ff
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-05 09:56:36 -0700

    F

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,10 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
-	if (s[0] != '\0')
+	if (!s)
 	{
+		return ('\0');
+	}
 		for (i = 0 ; s[i] != '\0' ; i++)
 		{
 		if (s[i] == c)
@@ -19,6 +20,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 		}
-	}
+
 	return ('\0');
 }
