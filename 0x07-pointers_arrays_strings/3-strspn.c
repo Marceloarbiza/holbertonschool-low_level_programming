9cdcc64a8c8189f82de330c0ee23fcb58183912f
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-05 10:16:11 -0700

    E

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,10 +8,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
 
 	for (i = 0 ; s[i] != ',' ; i++)
 	{
-		
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+			}
+		}
 	}
+	return (k);
 }
