9ae2a275867b516c74cf1abc0fbdbf54c7d326f2
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-07 11:03:31 -0700

    F

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - recurion
+ * comp - recurion
  * @n: int
+ * @j: int
  *
  * Return: int
  */
-int comp( int n , int j)
+int comp(int n, int j)
 {
 	if (j * j == n)
 	{
@@ -15,10 +16,17 @@ int comp( int n , int j)
 	{
 		return (-1);
 	}
-	return (comp(n , j + 1));
+	return (comp(n, j + 1));
 }
 
+#include "holberton.h"
+/**
+* _sqrt_recursion - recurion
+* @n: int
+*
+* Return: int
+*/
 int _sqrt_recursion(int n)
 {
-	return (comp(n , 1));
+	return (comp(n, 1));
 }
