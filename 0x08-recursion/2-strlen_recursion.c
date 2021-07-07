b42aae81bcb934a2737124286876cfa5a1abffff
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-07 08:44:03 -0700

    D

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - recursion
+ * @s: char
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = 0;
+
+	if (*s == '\0')
+	{
+		return (n);
+	}
+	else
+	{
+		n = 1 +	_strlen_recursion (++s);
+	}
+	return (n);
+}
