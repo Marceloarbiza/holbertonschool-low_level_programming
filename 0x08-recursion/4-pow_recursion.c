46f355c34578fac98a951307bfa7356c2739152d
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-07 09:36:59 -0700

    D

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - recursion
+ * @x: int
+ * @y: int
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+
+	x = x * _pow_recursion(x , y - 1);
+	return (x);
+}
