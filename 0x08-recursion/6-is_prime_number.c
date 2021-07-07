226d8cecec66b1422a83ab212df1cc9e774fdd30
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-07 11:27:34 -0700

    D

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * comp - recurion
+ * @n: int
+ * @j: int
+ *
+ * Return: int
+ */
+int comp(int n, int j)
+{
+	if (j * j == n)
+	{
+		return (j);
+	}
+	else if (j * j > n)
+	{
+		return (-1);
+	}
+	return (comp(n, j + 1));
+}
+
+#include "holberton.h"
+/**
+* _sqrt_recursion - recurion
+* @n: int
+*
+* Return: int
+*/
+int is_prime_number(int n)
+{
+	if (comp(n, 1) < 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
