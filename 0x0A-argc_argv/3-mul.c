909a59586315a1e9bfc97e0734d1343043220e5b
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-09 10:09:36 -0700

    F

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		i = *argv[1] - 48;
+		j = *argv[2] - 48;
+
+		k = i * j;
+
+		printf("%d\n", k);
+	}
+
+	return (0);
+}
