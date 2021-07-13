1dcc065f436ed163642fd5dce8c5b7991ce1c281
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-13 12:43:09 -0700

    F

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concat
+ *@s1: char
+ *@s2: char
+ *
+ *Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, m, len;
+	char *p = NULL;
+
+	if (s1 && s2)
+	{
+		for (i = 0 ; *(s1 + i) != '\0' ; i++)
+			;
+		
+		for (j = 0 ; *(s2 + j) != '\0' ; j++)
+			;
+
+		len = i + j;
+		p = malloc(sizeof(char) * (len + 1));
+
+		if (p)
+		{
+			k = 0;
+			m = 0;
+
+			for (k = 0 ; k < i ; k++)
+			{
+				*(p + k) = *(s1 + k);
+			}
+
+			for (k = i ; k < len ; k++)
+			{
+				*(p + k) = *(s2 + m);
+				m++;
+			}
+			
+			*(p + len) = '\0';
+		}
+	}
+	return (p);
+}
