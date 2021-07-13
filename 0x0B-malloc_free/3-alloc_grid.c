fbe4b5163b695858431d2f50fed0b26bf07e1de7
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-13 13:31:27 -0700

    F

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - return pointer
+ * @width: int
+ * @height: int
+ *
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matriz = NULL;
+	int i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return NULL;
+	}
+
+
+	matriz = (int**)malloc (width*sizeof(int *));
+
+	for (i = 0 ; i < width ; i++)
+	{
+		matriz[i] = (int *) malloc (height*sizeof(int));
+	}
+
+	return (matriz);
+}
