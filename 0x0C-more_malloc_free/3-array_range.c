285e71480291a1eba17cfe1b1486154f2defdab9
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-15 12:43:56 -0700

    F

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * array_range - range
+ * @min: int
+ * @max: int
+ *
+ * Return: in
+ */
+int *array_range(int min, int max)
+{
+	int *nray = NULL;
+	int i;
+	int dif = 0;
+
+	if (min > max)
+		return (NULL);
+
+	dif = max - min;
+
+	nray = malloc(sizeof(int) * dif);
+
+	if (!nray)
+		return (NULL);
+
+	for (i = 0 ; i < dif ; i++)
+	{
+		nray[i] = min;
+		min++;
+	}
+
+	return (nray);
+}
