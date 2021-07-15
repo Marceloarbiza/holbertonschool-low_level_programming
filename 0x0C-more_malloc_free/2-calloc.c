18cfe25984e570b3e62f0a87698cb90587e99f77
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-15 12:12:58 -0700

    D

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,31 +4,28 @@
 #include <string.h>
 /**
  * _calloc - calloc
+ *
  * @nmemb: unsigned int
  * @size: unsigned int
  *
+ * Return: p2
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void **p2 = NULL;
-	void *p1 = NULL;
+	char *p2 = NULL;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
-	
+		return (NULL);
+
 	p2 = malloc(size * nmemb);
 
 	if (!p2)
-		return NULL;
+		return (NULL);
 
 	for (i = 0 ; i < nmemb ; i++)
 	{
-		p1 = malloc(size * size);
-		
-		if (!p1)
-			return NULL;
-		p2[i] = p1;
+		p2[i] = 0;
 	}
 
 	return (p2);
