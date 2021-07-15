7d49f0a4d91134635524c26d01ee3cc693c63fd3
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-15 11:54:58 -0700

    F

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,5 +10,26 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void **p2 = NULL;
+	void *p1 = NULL;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return NULL;
 	
+	p2 = malloc(size * nmemb);
+
+	if (!p2)
+		return NULL;
+
+	for (i = 0 ; i < nmemb ; i++)
+	{
+		p1 = malloc(size * size);
+		
+		if (!p1)
+			return NULL;
+		p2[i] = p1;
+	}
+
+	return (p2);
 }
