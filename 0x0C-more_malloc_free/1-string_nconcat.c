a7d0e2c0ac2ba2a724405ca890ee8aec14cc10df
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-15 11:15:59 -0700

    F

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,16 +15,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int lenS1 = 0, lenS2 = 0, lenTot = 0, lenS3 = 0, lenS4 = 0;
 
 	if (s1)
-	{
 		for (lenS1 = 0 ; *(s1 + lenS1) != '\0' ; lenS1++)
 			;
-	}
 
 	if (s2)
-	{
 		for (lenS2 = 0 ; *(s2 + lenS2) != '\0' ; lenS2++)
 			;
-	}
 
 	if (n >= lenS2)
 	{
@@ -35,10 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	p = malloc(sizeof(char) * (lenTot + 1));
 
+	if (p == NULL)
+		return (NULL);
+
 	for (lenS3 = 0 ; lenS3 < lenS1 ; lenS3++)
-	{
 		p[lenS3] = s1[lenS3];
-	}
 
 	lenS3 = 0;
 
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - calloc
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
+}
