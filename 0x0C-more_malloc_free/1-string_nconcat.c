5d0b76402117a9feb0256353e26fe71d1dacae73
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-15 11:19:07 -0700

    F

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p = NULL;
 	unsigned int lenS1 = 0, lenS2 = 0, lenTot = 0, lenS3 = 0, lenS4 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	if (s1)
 		for (lenS1 = 0 ; *(s1 + lenS1) != '\0' ; lenS1++)
 			;
@@ -23,9 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			;
 
 	if (n >= lenS2)
-	{
 		n = lenS2;
-	}
 
 	lenTot = lenS1 + n;
 
@@ -44,7 +48,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[lenS4] = s2[lenS3];
 		lenS3++;
 	}
-
 	p[lenTot] = '\0';
 
 	return (p);
