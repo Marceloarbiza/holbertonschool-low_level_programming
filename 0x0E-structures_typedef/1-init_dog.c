873e587caadadd187b40e300cb72df6472cfaf27
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-19 11:21:09 -0700

    D

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	struct dog *d
+}
---
+++ 0x0E-structures_typedef/doc.h
@@ -0,0 +1,2 @@
+struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
