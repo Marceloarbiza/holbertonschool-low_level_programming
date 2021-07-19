08e91694325669812e5afbb9704bdccfbf9ff238
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-19 10:32:41 -0700

    F

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data dog
+ * @name: first name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: long desc
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
