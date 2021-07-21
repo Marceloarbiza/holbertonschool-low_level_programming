29c6f718f408da607c24b23fd73f123885dba74d
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-21 10:07:13 -0700

    D

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * int_index - int index
+  * @array: int
+  * @size: int
+  * @cmp: int
+  *
+  * Return: int
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, j, k = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			j = cmp(array[i]);
+
+			if (j == 1)
+				return (i);
+			k += j;
+		}
+
+		if (j == 0)
+			return (-1);
+	}
+	return (0);
+}
