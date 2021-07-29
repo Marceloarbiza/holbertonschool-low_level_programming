a82fca1d7426e28d1c8400308ccf994ea9423add
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-29 11:58:15 -0700

    D

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - add node
+ * @str: const char
+ * @head: list_t
+ * Return: list_t.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *node = NULL;
+	size_t num;
+
+	num = 0;
+	node = malloc(sizeof(list_t));
+
+	if (!num)
+	{
+		return (NULL);
+	}
+	else
+	{
+		node->str = strdup(str);
+	}
+
+	if (!node->str)
+	{
+		free(node);
+		return (NULL);
+	}
+	else
+
+	while (node->str)
+		num++;
+
+	node->len = num;
+	node->next = *head;
+	*head = node;
+
+	return (*head);
+
+}
