98d7e901225a6cbfc2158eda587c510ae09a3887
Marcelo Arbiza <3338@holbertonschool.com>
2021-07-29 10:42:15 -0700

    D

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+/**
+  * print_list - print list
+  * @h: const list_t
+  *
+  * Return: size_t
+  */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	size_t num;
+
+	tmp = h;
+	num = 0;
+
+	while (tmp != NULL)
+	{
+		if (tmp->str != NULL)
+		{
+			printf("[%d] %s\n", tmp->len, tmp->str);
+		}
+		else
+		{
+			printf("[0] (nill)\n");
+		}
+		tmp = tmp->next;
+		num++;
+	}
+	return (num);
+}
