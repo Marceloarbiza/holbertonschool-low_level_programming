72a65cf05da7d08ef86b3b49013d8d7b38d164a1
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-03 14:14:40 -0700

    D

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,24 +11,19 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp = NULL;
 	unsigned int i = 0;
+	listint_t *tmp = NULL;
 
 	tmp = head;
 
-	if (!head)
-		return (NULL);
-
-	if (head)
+	while (tmp && i != index)
 	{
-		while (tmp && (i != (index - 1)))
-		{
-			tmp = tmp->next;
-			i++;
-		}
-
-		if (tmp && (i == (index - 1)))
-			return (tmp);
+		tmp = tmp->next;
+		i++;
 	}
-	return (head);
+
+	if (tmp && i == index)
+		return (tmp);
+
+	return (NULL);
 }
