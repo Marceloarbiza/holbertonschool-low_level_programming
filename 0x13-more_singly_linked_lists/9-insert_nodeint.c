03a06038152645de7d36d0aa9cfe0f53f3dd1382
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-03 07:25:05 -0700

    D

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	else
 		new->n = n;
-	if (n == 0)
+	if (idx == 0)
 	{
 		new->next = h;
 		*head = new;
