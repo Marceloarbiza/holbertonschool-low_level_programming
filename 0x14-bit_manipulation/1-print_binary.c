7f34f55fc7510cdbf57c64feac8398d836adcde8
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-05 11:48:50 -0700

    D

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @n: uli
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >= 1)
+	{
+		print_binary(n / 2);
+		_putchar((n % 2) + '0');
+	}
+}
