626278ce5a31fe88c6c069aa4ad6615983d36bfc
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-05 11:33:10 -0700

    D

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - check the code
+ * @b: char
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	int sum = 0;
+	int mul = 1;
+
+	for (; b[i] ; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+		{
+			sum += mul;
+			mul *= 2;
+		}
+		else if (b[i] == '0')
+		{
+			mul *= 2;
+		}
+		i--;
+	}
+	return (sum);
+
+}
