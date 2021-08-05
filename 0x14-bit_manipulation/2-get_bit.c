6fcff1b640cbfd83b2d1e0f3db74373ae35ecf37
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-05 12:32:01 -0700

    D

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get-bit - check the code
+ * @index: index
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+	{
+		return (-1);
+	}
+
+	n = n >> index;
+
+	return (n & 1);
+}
