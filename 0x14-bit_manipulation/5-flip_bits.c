b44cc9c572b8d4522d9b12dbd20fc7e3651f72e4
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-05 19:11:00 -0700

    D

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - check the code
+ * @n: int
+ * @m: int
+ * Return: int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x, b; 
+
+	x = n ^ m, 
+	b = 0;
+
+	while (x > 0)
+	{
+		b = (x & 1);
+		x >>= 1;
+	}
+
+	return (b);
+}
