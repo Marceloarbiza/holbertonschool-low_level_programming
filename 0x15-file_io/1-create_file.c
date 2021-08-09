947d6ee609765295393c4df3405e4f9cf5bc16fd
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-09 10:53:57 -0700

    D

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	int op, wr;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
