a7dc9b90f0d405b2e282d2c58db540b0d9e27660
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-09 11:28:19 -0700

    D

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,14 +3,34 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * append_text_to_file - check the code
+ * @filename: char
+ * @text_content: char
+ * Return: int
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
+	int i = 0;
+	int op, wr;
+
+	if (!filename)
 		return (-1);
 
-	
+	op = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (op == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (; text_content[i] ; i++)
+			;
+
+		wr = write(op, text_content, i);
+	}
+	close(op);
+
+	if (wr != i)
+		return (-1);
+
+	return (1);
 }
