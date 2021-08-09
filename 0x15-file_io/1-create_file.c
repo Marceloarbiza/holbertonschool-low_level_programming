7759bcf386a0719bb7f6a6136b705d55daa920c1
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-09 10:22:57 -0700

    F

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - check the code
+ * @filename: char
+ * @text_content: char
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0;
+	int op, wr;
+
+	if (!filename)
+		return (-1);
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (text_content)
+	{
+		for (; text_content[i] ; i++)
+			;
+
+		wr = write(op, text_content, i);
+	}
+
+	if (wr != i)
+		return (-1);
+
+	return (1);
+}
