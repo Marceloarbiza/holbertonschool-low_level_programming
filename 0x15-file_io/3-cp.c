3fc1262f434c91a852edba654d49f3f355d78040
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-10 10:46:02 -0700

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,25 +4,41 @@
 
 /**
  * main - check the code
- *
+ * @argc: argc
+ * @argv: argv
  * Return: int
  */
 int main(int argc, char *argv[])
 {
 	int from, to, p = 1024;
 	char buf[1024 + 1];
-	
-	if (argc == 0)
-		return (-1);
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	from = open(argv[1], O_RDONLY);
+
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
 	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
 	while (p == 1024)
 	{
 		p = read(from, buf, 1024);
 		write(to, buf, p);
 	}
 
+	close(from);
 	return (to);
 }
