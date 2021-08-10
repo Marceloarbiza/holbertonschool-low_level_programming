135da3c354da0d86cebaaf601f055e06659d24e7
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-10 11:26:31 -0700

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,35 +10,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int from, to, p = 1024;
-	char buf[1024 + 1];
+	int from, to, wr, clf, clt, p = 1024;
+	char buf[1024];
 
-	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO,"Usage: cp %s %s\n", argv[1], argv[2]);
-		exit(97);
-	}
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	if (argv[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	from = open(argv[1], O_RDONLY);
-
-	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	to = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
 
 	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		to = open(argv[2], O_TRUNC | O_WRONLY);
+
 	while (p == 1024)
 	{
 		p = read(from, buf, 1024);
-		write(to, buf, p);
+		if (p == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		wr = write(to, buf, p);
+		if (wr == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 
-	close(from);
-	return (to);
+	clf = close(from);
+	clt = close(to);
+	if (clf == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+
+	if (clt == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+
+	return (0);
 }
