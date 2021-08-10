f56ca6c80692de36371a476b5e9a798754a2fa5a
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-10 11:29:11 -0700

    D

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,8 +17,10 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	if (argv[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	from = open(argv[1], O_RDONLY);
 	to = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
 
@@ -29,7 +31,10 @@ int main(int argc, char *argv[])
 	{
 		p = read(from, buf, 1024);
 		if (p == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		wr = write(to, buf, p);
 		if (wr == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
