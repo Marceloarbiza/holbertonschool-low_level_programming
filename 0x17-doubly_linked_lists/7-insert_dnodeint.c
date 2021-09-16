6d654a7e4cb1d13e1c2219747c30609980a73d86
Marceloarbiza <3338@holbertonschool.com>
2021-09-16 12:26:09 -0700

    D

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include "5-get_dnodeint.c"
 /**
  * insert_dnodeint_at_index - insert node at index
  * @h: head
@@ -10,8 +10,9 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
-
 	dlistint_t *tmp = *h;
+	dlistint_t *anterior = NULL;
+	dlistint_t *siguiente = NULL;
 	unsigned int count = 0;
 
 	if (!new)
@@ -21,9 +22,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new = add_dnodeint(h, n);
-		return (new);
+		free(new);
+		return (add_dnodeint(h, n));
 	}
+
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 
 	while (tmp)
 	{
@@ -37,12 +42,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else if (count == idx)
 	{
-		new = add_dnodeint_end(h, n);
-		return (new);
+		free(new);
+		return (add_dnodeint_end(h, n));
 	}
 	else
 	{
-
+		anterior = get_dnodeint_at_index(*h, idx);
+		siguiente = get_dnodeint_at_index(*h, idx + 1);
+		anterior->next = new;
+		new->prev = anterior;
+		new->next = siguiente;
+		siguiente->prev = new;
+		return (new);
 	}
 	return (new);
 }
