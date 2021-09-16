4e85b5337dfa983ece9757daa15d6e7cc5f0234c
Marceloarbiza <3338@holbertonschool.com>
2021-09-16 10:44:42 -0700

    D

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,14 +8,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = NULL;
 
-	if (head)
+	while (head)
 	{
-		while (head->next)
-		{
-			tmp = head->next;
-			free(head);
-			head = tmp;
-		}
-		free(tmp);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
+	free(tmp);
 }
