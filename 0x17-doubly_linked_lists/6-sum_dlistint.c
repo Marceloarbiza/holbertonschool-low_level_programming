6ff224acb04089fd0c07318f2f1ac380d0a7b56a
Marceloarbiza <3338@holbertonschool.com>
2021-09-16 11:15:37 -0700

    D

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum nodes
+ * @head: head
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
