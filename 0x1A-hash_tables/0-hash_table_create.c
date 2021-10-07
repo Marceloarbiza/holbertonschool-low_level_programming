60545121479ef8822fbba7d766552b200c5b8f6c
Marceloarbiza <3338@holbertonschool.com>
2021-10-07 10:08:09 -0700

    D

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,33 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable = NULL;
+	unsigned long int i = 0;
 
 	if (size < 1)
 	{
 		return (NULL);
 	}
 
-	hashtable = malloc(sizeof(hash_table_t) * size);
+	hashtable = malloc(sizeof(hash_table_t));
 
 	if (hashtable == NULL)
 	{
 		return (NULL);
 	}
+
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hashtable->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		hashtable->array = NULL;
+	}
+
+	hashtable->size = size;
 
 	return (hashtable);
 }
