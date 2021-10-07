773fc5808bdc14ae6ce2991444a75c7c70ac024a
Marceloarbiza <3338@holbertonschool.com>
2021-10-07 11:52:02 -0700

    D

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,33 +8,28 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable = NULL;
-	unsigned long int i = 0;
+	unsigned long int i;
 
-	if (size < 1)
-	{
+	if (size == 0)
 		return (NULL);
-	}
 
 	hashtable = malloc(sizeof(hash_table_t));
 
 	if (hashtable == NULL)
-	{
 		return (NULL);
-	}
 
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hashtable->array == NULL)
 	{
+		free(hashtable);
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
-	{
-		hashtable->array[i] = NULL;
-	}
-
 	hashtable->size = size;
+
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
 
 	return (hashtable);
 }
