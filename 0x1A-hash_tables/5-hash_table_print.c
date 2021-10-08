203cc4232b514da4563bed6d87fcd87679b07ab7
Marceloarbiza <3338@holbertonschool.com>
2021-10-08 11:50:32 -0700

    D

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,7 +6,9 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *arrtmp = NULL;
 	unsigned long int i;
+	unsigned long int flag = 0;
 
 	if (ht)
 	{
@@ -14,7 +16,20 @@ void hash_table_print(const hash_table_t *ht)
 		for (i = 0; i < ht->size; i++)
 		{
 			if (ht->array[i])
-				printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+			{
+				arrtmp = ht->array[i];
+
+				while (arrtmp)
+				{
+					if (flag == 1)
+						printf(", ");
+
+					printf("'%s': '%s'", arrtmp->key, arrtmp->value);
+					flag = 1;
+					arrtmp = arrtmp->next;
+				}
+			}
+			flag++;
 		}
 		printf("}\n");
 	}
