c73154a1bea0f7680295c34120c934430dbb0f5a
Marceloarbiza <3338@holbertonschool.com>
2021-10-08 10:36:13 -0700

    D

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: the key
+ * Return: value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *arrtmp = NULL;
+
+	if (!ht || !key)
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	arrtmp = ht->array[idx];
+
+	while (arrtmp)
+	{
+		if (strcmp(arrtmp->key, key) == 0)
+			return (arrtmp->value);
+	}
+
+	return (NULL);
+}
