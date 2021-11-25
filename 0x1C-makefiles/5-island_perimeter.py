fbe2647656e20582a8e3ad1044afb9b0bff9e184
Marcelo <3338@holbertonschool.com>
2021-11-25 11:04:06 -0800

    D

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j-1] == 0:
+                    p +=1
+                if grid[i][j+1] == 0:
+                    p +=1
+                if grid[i-1][j] == 0:
+                    p +=1
+                if grid[i+1][j] == 0:
+                    p+=1
+    return p
