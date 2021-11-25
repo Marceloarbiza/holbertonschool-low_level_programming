0528897d731942f85fc1f1c90f645ad56d0c008f
Marcelo <3338@holbertonschool.com>
2021-11-25 11:07:12 -0800

    D

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+    return
+    the
+    perimeter
+"""
 
 def island_perimeter(grid):
+    """ primeter of go to hell """
     p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
