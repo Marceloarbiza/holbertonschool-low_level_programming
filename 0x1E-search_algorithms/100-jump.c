f53e1aaaba9166e69063ac6ec27fd2d0112f3eb5
Marcelo <3338@holbertonschool.com>
2022-01-19 08:07:28 -0800

    100j

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -23,7 +23,10 @@ int jump_search(int *array, size_t size, int value)
 		return (-1);
 
 	if (array[i] == value)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		return (i);
+	}
 
 	for (i = 0; i < size_i; i += sqrt_int)
 	{
